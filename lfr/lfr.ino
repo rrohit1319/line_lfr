4bfdeff6f29b3b82a8f88aa6638dfb540daf0d2b
rrohit1319 <123386235+rrohit1319@users.noreply.github.com>
2025-04-29 03:16:11 +0530

    Add files via upload

---
+++ lfr/lfr.ino
@@ -0,0 +1,68 @@
+#define LEFT_SENSOR 11
+#define RIGHT_SENSOR 12
+#define MOTOR_LEFT_FORWARD 5
+#define MOTOR_LEFT_BACKWARD 6
+#define MOTOR_RIGHT_FORWARD 9
+#define MOTOR_RIGHT_BACKWARD 10
+#define Left_LED 2
+#define Right_LED 3
+int speed = 150;
+
+void setup() {
+    pinMode(LEFT_SENSOR, INPUT);
+    pinMode(RIGHT_SENSOR, INPUT);
+    pinMode(MOTOR_LEFT_FORWARD, OUTPUT);
+    pinMode(MOTOR_LEFT_BACKWARD, OUTPUT);
+    pinMode(MOTOR_RIGHT_FORWARD, OUTPUT);
+    pinMode(MOTOR_RIGHT_BACKWARD, OUTPUT);
+}
+
+void loop() {
+    int leftState = digitalRead(LEFT_SENSOR);
+    int rightState = digitalRead(RIGHT_SENSOR);
+    if (leftState == LOW && rightState == LOW) {
+        moveForward();
+    } else if (leftState == HIGH && rightState == LOW) {
+        turnRight();
+    } else if (leftState == LOW && rightState == HIGH) {
+        turnLeft();
+    } else {
+        stopMotors();
+    }
+}
+
+void moveForward() {
+    digitalWrite(MOTOR_LEFT_FORWARD, speed);
+    digitalWrite(MOTOR_LEFT_BACKWARD, LOW);
+    digitalWrite(MOTOR_RIGHT_FORWARD, speed);
+    digitalWrite(MOTOR_RIGHT_BACKWARD, LOW);
+    digitalWrite(Left_LED, LOW);
+    digitalWrite(Right_LED, LOW);
+}
+
+void turnRight() {
+    digitalWrite(MOTOR_LEFT_FORWARD, speed);
+    digitalWrite(MOTOR_LEFT_BACKWARD, LOW);
+    digitalWrite(MOTOR_RIGHT_FORWARD, LOW);
+    digitalWrite(MOTOR_RIGHT_BACKWARD, speed);
+    digitalWrite(Left_LED, LOW);
+    digitalWrite(Right_LED, HIGH);
+}
+
+void turnLeft() {
+    digitalWrite(MOTOR_LEFT_FORWARD, LOW);
+    digitalWrite(MOTOR_LEFT_BACKWARD, speed);
+    digitalWrite(MOTOR_RIGHT_FORWARD, speed);
+    digitalWrite(MOTOR_RIGHT_BACKWARD, LOW);
+    digitalWrite(Left_LED, HIGH);
+    digitalWrite(Right_LED, LOW);
+}
+
+void stopMotors() {
+    digitalWrite(MOTOR_LEFT_FORWARD, LOW);
+    digitalWrite(MOTOR_LEFT_BACKWARD, LOW);
+    digitalWrite(MOTOR_RIGHT_FORWARD, LOW);
+    digitalWrite(MOTOR_RIGHT_BACKWARD, LOW);
+    digitalWrite(Left_LED, HIGH);
+    digitalWrite(Right_LED, HIGH);
+}
