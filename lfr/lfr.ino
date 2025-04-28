25028252731a580ee2373267b67cd78ba31c825a
rrohit1319 <123386235+rrohit1319@users.noreply.github.com>
2025-04-29 04:11:56 +0530

    Update lfr.ino

--- lfr/lfr.ino
+++ lfr/lfr.ino
@@ -1,34 +1,48 @@
-#define LEFT_SENSOR 11
-#define RIGHT_SENSOR 12
-#define MOTOR_LEFT_FORWARD 5
-#define MOTOR_LEFT_BACKWARD 6
+// Sensor and Motor Pin Definitions
+#define LEFT_SENSOR 12
+#define RIGHT_SENSOR 13
+
+#define MOTOR_LEFT_FORWARD 11
+#define MOTOR_LEFT_BACKWARD 10
 #define MOTOR_RIGHT_FORWARD 9
-#define MOTOR_RIGHT_BACKWARD 10
+#define MOTOR_RIGHT_BACKWARD 8
+
 #define Left_LED 2
 #define Right_LED 3
-#define ENA_1 A0
-#define ENA_2 A1
-int speed = 150;
+
+#define ENA_1 5  // PWM pin (previously A0, corrected)
+#define ENA_2 6   // Use 4 as digital HIGH/LOW (or if needed, you can move to PWM pin 6, 9, 10, 11)
+
+int speedControl = 100; // Motor speed (0-255)
 
 void setup() {
+    // Sensor Pins
     pinMode(LEFT_SENSOR, INPUT);
     pinMode(RIGHT_SENSOR, INPUT);
+
+    // Motor Control Pins
     pinMode(MOTOR_LEFT_FORWARD, OUTPUT);
     pinMode(MOTOR_LEFT_BACKWARD, OUTPUT);
     pinMode(MOTOR_RIGHT_FORWARD, OUTPUT);
     pinMode(MOTOR_RIGHT_BACKWARD, OUTPUT);
+
+    // Enable Pins
     pinMode(ENA_1, OUTPUT);
     pinMode(ENA_2, OUTPUT);
+
+    // LEDs
     pinMode(Right_LED, OUTPUT);
     pinMode(Left_LED, OUTPUT);
 
-    analogWrite(ENA_1, 100);
-    analogWrite(ENA_2, 100);
+    // Set Motor Speed
+    analogWrite(ENA_1, speedControl);
+    digitalWrite(ENA_2, HIGH);  // If ENA_2 is on non-PWM pin like D4, just set HIGH for full speed
 }
 
 void loop() {
     int leftState = digitalRead(LEFT_SENSOR);
     int rightState = digitalRead(RIGHT_SENSOR);
+
     if (leftState == LOW && rightState == LOW) {
         moveForward();
     } else if (leftState == HIGH && rightState == LOW) {
@@ -38,6 +52,8 @@ void loop() {
     } else {
         stopMotors();
     }
+
+    delay(5); // Small delay to stabilize readings
 }
 
 void moveForward() {
@@ -50,6 +66,7 @@ void moveForward() {
 }
 
 void turnRight() {
+    // Tight turn (left wheel forward, right wheel backward)
     digitalWrite(MOTOR_LEFT_FORWARD, HIGH);
     digitalWrite(MOTOR_LEFT_BACKWARD, LOW);
     digitalWrite(MOTOR_RIGHT_FORWARD, LOW);
@@ -59,6 +76,7 @@ void turnRight() {
 }
 
 void turnLeft() {
+    // Tight turn (left wheel backward, right wheel forward)
     digitalWrite(MOTOR_LEFT_FORWARD, LOW);
     digitalWrite(MOTOR_LEFT_BACKWARD, HIGH);
     digitalWrite(MOTOR_RIGHT_FORWARD, HIGH);
